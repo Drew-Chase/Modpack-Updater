0da2acf01b46d4a1709c3cf6cf8dcda98409c45a
Drew Chase <dcmanproductions@gmail.com>
2022-04-25 00:13:20 -0400

    Added Network Util

---
+++ src/main/java/com/drewchaseproject/mc/modpack_updater/Utils/NetworkUtil.java
@@ -0,0 +1,47 @@
+package com.drewchaseproject.mc.modpack_updater.Utils;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Path;
+import java.util.Map;
+
+import com.drewchaseproject.mc.modpack_updater.Objects.HttpConnection;
+
+public class NetworkUtil {
+    public static boolean DownloadFile(URL url, Path file) {
+        File f = file.toFile();
+        f.getParentFile().mkdirs();
+        try (BufferedInputStream in = new BufferedInputStream(url.openStream())) {
+            FileOutputStream output = new FileOutputStream(f);
+            byte buffer[] = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(buffer, 0, 1024)) != -1) {
+                output.write(buffer, 0, bytesRead);
+            }
+            output.close();
+            in.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static HttpConnection MakeConnection(URL url, Map<String, String> headers) {
+        try {
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestProperty("User-Agent", "Minecraft Modpack Updater");
+            for (String key : headers.keySet()) {
+                http.setRequestProperty(key, headers.get(key));
+            }
+            return new HttpConnection(http);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
