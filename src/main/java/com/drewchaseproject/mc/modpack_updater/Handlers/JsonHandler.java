e2c640f538622b6ffb42e40018071681364f2b3c
Drew Chase <dcmanproductions@gmail.com>
2022-04-26 13:16:57 -0400

    Added Method to write json to file

--- src/main/java/com/drewchaseproject/mc/modpack_updater/Handlers/JsonHandler.java
+++ src/main/java/com/drewchaseproject/mc/modpack_updater/Handlers/JsonHandler.java
@@ -1,17 +1,20 @@
 package com.drewchaseproject.mc.modpack_updater.Handlers;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Path;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class JsonHandler {
 
-    public static JsonElement ParseJsonFromFile(Path file) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(file.toFile()))) {
+    public static JsonElement ParseJsonFromFile(File file) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
 
             String line = reader.readLine();
             StringBuilder builder = new StringBuilder();
@@ -27,6 +30,17 @@ public class JsonHandler {
         }
 
         return null;
+    }
+
+    public static void WriteJsonToFile(JsonObject json, File file) {
+        file.getParentFile().mkdirs();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write(json.toString());
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
