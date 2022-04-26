3d93d67a51673f1bf02f2c86d7b7ae0a99dba265
Drew Chase <dcmanproductions@gmail.com>
2022-04-26 13:20:02 -0400

    Removed ID and added Project and File ID's

--- src/main/java/com/drewchaseproject/mc/modpack_updater/Objects/Mod.java
+++ src/main/java/com/drewchaseproject/mc/modpack_updater/Objects/Mod.java
@@ -1,18 +1,21 @@
 package com.drewchaseproject.mc.modpack_updater.Objects;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Path;
 
+import com.drewchaseproject.mc.modpack_updater.App;
 import com.drewchaseproject.mc.modpack_updater.Handlers.NetworkHandler;
 import com.google.gson.JsonObject;
 
 public class Mod {
-    private int ID;
     private String Name;
     private String FileName;
+    private int ProjectID;
+    private int FileID;
     private URL DownloadURL;
 
     public Mod(int projectID, int fileID) {
@@ -21,7 +24,9 @@ public class Mod {
             HttpConnection connection = new HttpConnection((HttpURLConnection) new URL(String.format("https://cursemeta.dries007.net/%s/%s.json", projectID, fileID)).openConnection());
             JsonObject json = connection.GetContentAsJson();
 
-            ID = json.get("Id").getAsInt();
+            ProjectID = projectID;
+            FileID = fileID;
+
             Name = json.get("DisplayName").getAsString();
             FileName = json.get("FileName").getAsString();
             DownloadURL = new URL(json.get("DownloadURL").getAsString());
@@ -33,15 +38,26 @@ public class Mod {
         }
     }
 
-    public int GetID() {
-        return ID;
+    public int GetProjectID() {
+        return ProjectID;
+    }
+
+    public int GetFileID() {
+        return FileID;
     }
 
     public String GetName() {
         return Name;
     }
 
-    public boolean Download(Path directory) {
-        return NetworkHandler.DownloadFile(DownloadURL, Path.of(directory.toAbsolutePath().toString(), FileName));
+    public String GetFileName() {
+        return FileName;
+    }
+
+    public boolean Download() {
+        App.log.info(String.format("Downloading \"%s\"", FileName));
+        Path outputDirectory = Path.of(App.GetInstance().WorkingDirectory.toAbsolutePath().toString(), "temp", "toIntall");
+        outputDirectory.toFile().mkdirs();
+        return NetworkHandler.DownloadFile(DownloadURL, Path.of(outputDirectory.toAbsolutePath().toString(), FileName));
     }
 }
