c26636a6e398b2a72db5b688b426a22ec9e30fe9
Drew Chase <dcmanproductions@gmail.com>
2022-04-26 20:38:41 -0400

    Added Cloth

---
+++ src/main/java/com/drewchaseproject/mc/fabric/modpack_updater/ClothScreenManager.java
@@ -0,0 +1,32 @@
+package com.drewchaseproject.mc.fabric.modpack_updater;
+
+import com.drewchaseproject.mc.modpack_updater.App;
+
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.clothconfig2.api.ConfigCategory;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.TranslatableText;
+
+public class ClothScreenManager {
+
+    public static Screen CreateConfigScreen(Screen parent) {
+
+        // Create Screen
+        ConfigBuilder builder = ConfigBuilder.create().setTitle(new TranslatableText(String.format("title.%s.config", App.GetInstance().MOD_ID)));
+        builder.setParentScreen(parent);
+        // Create Category
+        ConfigCategory general = builder.getOrCreateCategory(new TranslatableText(String.format("category.%s.general", App.GetInstance().MOD_ID)));
+
+        // Add Project ID Option
+        ConfigEntryBuilder entryBuilder = builder.entryBuilder();
+        general.addEntry(entryBuilder.startIntField(new TranslatableText(String.format("option.%s.projectID", App.GetInstance().MOD_ID)), App.GetInstance().config.GetProjectID()).setDefaultValue(0).setTooltip(new TranslatableText(String.format("option.%s.projectID.tooltip", App.GetInstance().MOD_ID))).setSaveConsumer(newValue -> App.GetInstance().config.SetProjectID(newValue)).build());
+        
+
+        // Saving
+        builder.setDoesConfirmSave(true);
+
+        return builder.build();
+    }
+
+}
